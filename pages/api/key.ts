25cdad4c2287c3cce81c9e3e1c624f7fd07fa0d0
squirrelcoding <rosilescarlos28@gmail.com>
2022-03-15 20:34:53 -0500

    tried to fix bug

--- pages/api/key.ts
+++ pages/api/key.ts
@@ -1,9 +1,11 @@
 /** Database key */
+const { privateKey } = JSON.parse(process.env.FIREBASE_KEY_PRIVATE_KEY!);
+
 const key = {
     type: "service_account",
     project_id: process.env.FIREBASE_PROJECT_ID!,
     private_key_id: process.env.FIREBASE_KEY_PRIVATE_ID!,
-    private_key: process.env.FIREBASE_KEY_PRIVATE_KEY!,
+    private_key: privateKey,
     client_email: process.env.FIREBASE_CLIENT_EMAIL!,
     client_id: process.env.FIREBASE_CLIENT_ID!,
     auth_uri: "https://accounts.google.com/o/oauth2/auth",
